01774a704dc6f709007f7f53c763511e8d7cfb51
Shekhar <shekharyadav638@gmail.com>
2024-08-15 21:07:43 +0530

    ass-03-15: Landing page completed

--- js/heroSlider.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  $('.slide').slick({
-    infinite: true,
-    autoplaySpeed: 1500,
-    autoplay: true,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-  });
-});
